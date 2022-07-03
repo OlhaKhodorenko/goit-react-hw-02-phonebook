5939ff3c57232828bfbb51cabbefd919587676b6
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-07-03 17:50:48 +0300

    add

---
+++ src/components/contactForm/contactForm.jsx
@@ -0,0 +1,73 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import css from './contactsForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  loginInputId = nanoid();
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onFormSubmit(this.state);
+    this.reset();
+    console.log(this.state);
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.contactForm} onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            className={css.input}
+            onChange={this.handleChange}
+            id={this.loginInputId}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            className={css.input}
+            onChange={this.handleChange}
+            pattern="[\+]\d{3}\s[\(]\d{2}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="input" className={css.submitBtn}>
+          Add contacts
+        </button>
+      </form>
+    );
+  }
+}
+export default ContactForm;
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
